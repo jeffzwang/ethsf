a5998831d8d783ae630988054e21460ca4409bb8
James Kim <jakim929@gmail.com>
2022-11-06 03:06:01 -0800

    progress

--- frontend/components/ResultsPanel.tsx
+++ frontend/components/ResultsPanel.tsx
@@ -1,16 +1,19 @@
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 import couchPic from '../public/bluecouchphoto.jpg';
+import couchPic2 from '../public/couchpic2.png';
+import couchPic3 from '../public/couchpic3.png';
+import pfpPic2 from '../public/pfp2.avif'
 import pfpPic from '../public/pfp.jpg';
 import { MapPinIcon, HandThumbUpIcon, BoltIcon } from '@heroicons/react/24/solid';
 import { useSigner, useContractRead, useAccount } from 'wagmi';
 import { ethers } from 'ethers';
 import { StayPlatformAbi, StayPlatformAddress } from '/deployments/StayPlatform';
 import { VerifierNFTAbi, VerifierNFTAddress } from '/deployments/VerifierNFT';
-import { useEffect, useState } from 'react';
 import { sendRequestToBook } from '/lib/sendRequestToBook';
-import Router, { useRouter } from 'next/router';
+import { useRouter } from 'next/router';
 import GreenButton from '/components/GreenButton';
 import USDCIcon from '/icons/USDCIcon';
+import { getAddressForDisplay } from '/lib/getAddressForDisplay';
 
 const useIsEligibleForInstantBook = () => {
   const { address } = useAccount();
@@ -28,24 +31,38 @@ const useIsEligibleForInstantBook = () => {
   return false;
 }
 
-const ListingCard = () => {
+const ListingCard = ({
+  listingPic,
+  hostPic,
+  host,
+  location,
+  description,
+  hostAddress
+}: {
+  listingPic: StaticImageData,
+  hostPic: StaticImageData,
+  host?: string,
+  location: string,
+  description: string,
+  hostAddress: string,
+}) => {
   const { data: signer } = useSigner();
   const { address } = useAccount();
   const router = useRouter();
 
   return (
     <div className="flex bg-white">
-      <div className="w-[220px]">
-        <Image src={couchPic} alt="picture of listing" />
+      <div className="w-[220px] relative">
+        <Image src={listingPic} alt="picture of listing" fill />
       </div>
       <div className="flex-1 flex flex-col">
         <div className="flex">
-          <div className="w-[96px] h-[96px]">
-            <Image src={pfpPic} alt="picture of host" />
+          <div className="w-[96px] h-[96px] relative">
+            <Image src={hostPic} alt="picture of host" fill />
           </div>
           <div className="flex-1 flex flex-col p-1 pl-4">
             <div className="text-2xl font-semibold py-2 font-mono">
-              Ashley.eth
+              {host ? host : getAddressForDisplay(hostAddress)}
             </div>
             <div className="flex space-x-8">
               <div className="flex space-x-2">
@@ -75,14 +92,15 @@ const ListingCard = () => {
             <div className="flex space-x-3">
               <MapPinIcon className="h-5 w-5" />
               <div className="font-semibold">
-                Marina District, SF
+                {location}
+
               </div>
             </div>
             <div>
               Couch in the living room
             </div>
             <div className="text-sm">
-              20 min walk to the EthSF hackathon venue. Nice couch that also converts to a futon. I'm looking to make new frens in crypto, while supporting myself to make art
+              {description}
             </div>
           </div>
         </div>
@@ -108,7 +126,7 @@ const ListingCard = () => {
         </div>
         <div className="flex flex-col space-y-2">
           <GreenButton
-            className="opacity-80 bg-gray-400 shadow-none items-center flex space-x-2 justify-center"
+            className="opacity-80 bg-gray-400 shadow-none items-center flex space-x-2 justify-center text-sm"
             onClick={async () => {
               const contract = new ethers.Contract(StayPlatformAddress, StayPlatformAbi, signer!);
               await contract.createStayTransaction(0, 1, 0, '0x0', 3, '0x0', 'some_ipfs', 111, '', '');
@@ -119,6 +137,7 @@ const ListingCard = () => {
             </div>
           </GreenButton>
           <GreenButton
+            className="text-sm"
             onClick={async () => {
               if (!signer || !address) {
                 return;
@@ -149,8 +168,22 @@ const ListingCard = () => {
 const ResultsPanel = () => {
   return (
     <div className="bg-lightSky space-y-2 p-2 rounded-md">
-      <ListingCard />
-      <ListingCard />
+      <ListingCard
+        listingPic={couchPic}
+        host="tectonicplate.eth"
+        hostAddress="0xc942c9a53012a24c466718f37B31Ed5251a06982"
+        hostPic={pfpPic}
+        location="Marina District, SF"
+        description="20 min walk to the EthSF hackathon venue. Nice couch that also converts to a futon. I'm looking to make new frens in crypto, while supporting myself to make art"
+      />
+      <ListingCard
+        listingPic={couchPic2}
+        hostAddress="0xc942c9a53012a24c466718f37B31Ed5251a06982"
+        hostPic={pfpPic2}
+        location="Mission, SF"
+        description="I’m looking to make crypto friends! I have two dogs and a cat, so if you’re allergic my place isn’t for you. "
+
+      />
     </div>
   );
 };
